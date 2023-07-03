6e7f6ab28785b14a6b108c97a72ef2d787c06c26
murtazasani <121139736+murtazasani@users.noreply.github.com>
2023-07-03 20:33:57 +0500

    Create day5_part2.py

---
+++ day5_part2.py
@@ -0,0 +1,37 @@
+# Some tuple functions
+
+
+tup = (39, 59, 1, 93, 82)
+
+tup1 = ("sting", "jing", [1,2,3,4])
+print(type(tup))
+print(tup1[0])
+print(tup1[2][2])
+
+x = (1, 3, 4) + (1, 3, 4)
+print(x)
+
+# Some String Functions
+
+my_str = "hello"
+print(my_str)
+
+my_str1 = '''
+Hello
+World
+How 
+is 
+Everything 
+Going 
+On?
+'''
+print(my_str1)
+
+# String Slicing
+
+my_str2 = "Hello My Name is Muhammad Murtaza"
+print(my_str2[0:16])
+print(my_str2[-1])
+print(my_str2[0:16:3])
+print(my_str2[:-15:-1])
+print(my_str2[::-1])
