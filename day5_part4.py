bf9cc9ac93e19f3522aada45e312dd8378060aa7
murtazasani <121139736+murtazasani@users.noreply.github.com>
2023-07-03 20:34:27 +0500

    Create day5_part4.py

---
+++ day5_part4.py
@@ -0,0 +1,16 @@
+# Some Functions on Dictionary
+
+dob = {"Date": 11, "Month": 2, "Year": 1998}
+
+print(dob)
+print(dob.keys())
+print(dob.values())
+print(dob["Date"])
+
+groclis = {"Fruits": ["Apple", "Mango", "Banana"],
+           "Vegetables": ["Tomato", "Potato", "Carrot"]}
+print(groclis)
+print(groclis.keys())
+print(groclis.values())
+print(groclis["Fruits"])
+print(groclis["Vegetables"][1])
