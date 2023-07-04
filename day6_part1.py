32f57ff674764e40b358e79007b89e589fe72b77
murtazasani <121139736+murtazasani@users.noreply.github.com>
2023-07-04 08:26:46 +0500

    Create day6_part1.py

---
+++ day6_part1.py
@@ -0,0 +1,17 @@
+# Distance Conversion From Kilometers to Miles.
+
+km = float(input("Enter Distance in Kilometers: "))
+
+mile = km / 1.609344
+
+print(km, " Kilometer in miles is = ", (round(mile, 3)))
+
+
+# Simple Program to Calculate Area and Circumference
+
+radius = float(input("Enter the radius: "))
+area = 3.14 * radius ** 2
+circumference = 2 * 3.14 * radius
+
+print("Area of Circle is =", area)
+print("Area of Circumference =", circumference)
