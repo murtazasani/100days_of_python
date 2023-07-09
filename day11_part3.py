624512cafbda0f9856d859135bb46cb0a18b2c86
murtazasani <121139736+murtazasani@users.noreply.github.com>
2023-07-09 21:34:11 +0500

    Create day11_part3.py

---
+++ day11_part3.py
@@ -0,0 +1,24 @@
+
+
+try:
+    number1 = input("Enter a Number: ")
+    number1 = float(number1)
+
+except:
+    print("You have ValueError. \nNote:(ValueError) occur when you entered text but it required Number.")
+    number1 = input("Enter a Number: ")
+
+try:
+    number2 = input("Enter a Another Number: ")
+    number2 = float(number2)
+
+except:
+    print("You have ValueError. \nNote:(ValueError) occur when you entered text but it required Number.")
+    number2 = input("Enter a Another Number: ")
+
+if number1 < number2:
+    print("Number 2 is Greater.")
+elif number1 > number2:
+    print("Number 1 is Greater.")
+else:
+    print("Number 1 is Equal to Number 2.")
