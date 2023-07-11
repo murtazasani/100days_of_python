4c7075db7b92cbf84be3a453e65e95c23839b670
murtazasani <121139736+murtazasani@users.noreply.github.com>
2023-07-11 21:27:08 +0500

    Create day13_part1.py

---
+++ day13_part1.py
@@ -0,0 +1,20 @@
+# Program to perform all Assignment Operations
+
+x = int(input("Enter a Number: "))
+x += 3
+print("Addition is:", x)
+a = x
+x -= 4
+print("Subtraction is:", x)
+s = x
+x /= 2
+print("Division is:", x)
+d = x
+x *= 3
+print("Multiplication is:", x)
+m = x
+x %= 3
+print("Modulus is:", x)
+m2 = x
+sum1 = a + s + m + d + m2
+print(sum1)
