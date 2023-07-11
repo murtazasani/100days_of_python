4969318387bdb8b09c7c0d488218f65c86101a1c
murtazasani <121139736+murtazasani@users.noreply.github.com>
2023-07-11 21:27:18 +0500

    Create day13_part2.py

---
+++ day13_part2.py
@@ -0,0 +1,22 @@
+# Printing Alphabets in a Beautiful Shape
+
+print("A")
+print("B \t C")
+print("D \t E \t F")
+print("G \t H \t I \t J")
+print("K \t L \t M \t N \t O\t Z")
+print("P \t Q \t R \t S")
+print("T \t U \t V")
+print("W \t X")
+print("Y")
+
+print("\n")
+
+print("A")
+print("B C")
+print("D E F")
+print("G H I J")
+print("K L M N O")
+print("P Q R S T U")
+print("V W X Y Z 1 2")
+
