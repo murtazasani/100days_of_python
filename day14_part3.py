fb1d1e49e8eeef7b0dd869d22c8fdbd231185725
murtazasani <121139736+murtazasani@users.noreply.github.com>
2023-07-12 23:17:04 +0500

    Create day14_part3.py

---
+++ day14_part3.py
@@ -0,0 +1,16 @@
+# Program to check alphabet is vowel
+
+alph = input("Enter the Alphabet: ")
+
+if alph == "a":
+    print("Alphabet is Vowel.")
+elif alph == "e":
+    print("Alphabet is Vowel.")
+elif alph == "i":
+    print("Alphabet is Vowel.")
+elif alph == "o":
+    print("Alphabet is Vowel.")
+elif alph == "u":
+    print("Alphabet is Vowel.")
+else:
+    print("Alphabet is Consonant.")
