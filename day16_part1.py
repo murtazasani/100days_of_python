47dc1be92828130f3faa4b7a8c59ab2a8f37b33d
murtazasani <121139736+murtazasani@users.noreply.github.com>
2023-07-18 23:04:05 +0500

    Create day16_part1.py

---
+++ day16_part1.py
@@ -0,0 +1,9 @@
+alph = input("Enter a Alphabet: ")
+
+if (alph == "a") or (alph == 'e') or (alph == "i") or (alph == "o") or (alph == "u"):
+    print("Alphabet is Vowel.")
+elif (alph == "1") or (alph == '2') or (alph == "3") or (alph == "4") or (alph == "5") or\
+        (alph == "6") or (alph == '7') or (alph == "8") or (alph == "9") or (alph == "0"):
+    print("It's Number Not Alphabet")
+else:
+    print("Alphabet is consonant.")
