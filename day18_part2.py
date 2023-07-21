2ed8b8496df6b6d497c13cefe104ac2cd0ef2d69
murtazasani <121139736+murtazasani@users.noreply.github.com>
2023-07-21 20:02:19 +0500

    Create day18_part2.py

---
+++ day18_part2.py
@@ -0,0 +1,12 @@
+# To Find Cube of Number
+
+number = int(input("Enter a Number: "))
+square = number * number
+cube = number ** 3
+p4 = number ** 2 ** 2
+p5 = number ** 2 ** 3
+
+print("Square of", number, "=", square)
+print("Cube of", number, "=", cube)
+print("Power 4 of", number, "=", p4)
+print("Power 5 of", number, "=", p5)
