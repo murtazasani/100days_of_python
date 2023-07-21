39518c7d0a8ff5e4f13b8af51049df4bc51b87d0
murtazasani <121139736+murtazasani@users.noreply.github.com>
2023-07-21 20:02:27 +0500

    Create day18_part3.py

---
+++ day18_part3.py
@@ -0,0 +1,10 @@
+# To Check if Number is Positive or Negative
+
+number = float(input("Enter the Number: "))
+
+if number < 0:
+    print("Number is Negative.")
+elif number == 0:
+    print("Number is Neither Positive Nor Negative. It is Neutral Number.")
+else:
+    print("Number is Positive.")
